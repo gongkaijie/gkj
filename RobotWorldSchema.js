5e49862d46fed701794b76a24d99f86b8c4cb25a
gongkaijie <73537579+gongkaijie@users.noreply.github.com>
2020-10-27 22:01:12 +0800

    这是啥
    第一次联系

---
+++ RobotWorldSchema.js
@@ -0,0 +1,28 @@
+"use strict";
+/*---------------------------------------------------------------------------------------------
+* Copyright (c) Bentley Systems, Incorporated. All rights reserved.
+* See LICENSE.md in the project root for license terms and full copyright notice.
+*--------------------------------------------------------------------------------------------*/
+// __PUBLISH_EXTRACT_START__ Schema.schemaNames
+exports.__esModule = true;
+exports.Category = exports.CodeSpec = exports.Class = void 0;
+/** Constants associated with RobotWorld schema. */
+/** The "class full names" (SchemaName:ClassName) for the classes associated with RobotWorld. */
+var Class;
+(function (Class) {
+    Class["Robot"] = "RobotWorld:Robot";
+    Class["Barrier"] = "RobotWorld:Barrier";
+})(Class = exports.Class || (exports.Class = {}));
+/** The CodeSpec names associated with RobotWorld. */
+var CodeSpec;
+(function (CodeSpec) {
+    CodeSpec["Robot"] = "RobotWorld:Robot";
+    CodeSpec["Barrier"] = "RobotWorld:Barrier";
+})(CodeSpec = exports.CodeSpec || (exports.CodeSpec = {}));
+/** The SpatialCategory names associated with RobotWorld. */
+var Category;
+(function (Category) {
+    Category["Robot"] = "RobotWorld:Robot";
+    Category["Barrier"] = "RobotWorld:Barrier";
+})(Category = exports.Category || (exports.Category = {}));
+// __PUBLISH_EXTRACT_END__
